f25a99b904dc2b19a6c7a9dd0e702a7edd8fd3e4
carlos.melero <cmelero96@gmail.com>
2021-11-09 19:25:27 +0100

    Card can now flip and store children by props

--- src/App.js
+++ src/App.js
@@ -4,7 +4,17 @@ import FlipCard from './components/ui/FlipCard';
 function App() {
   return (
     <div id="App">
-      <FlipCard></FlipCard>
+      <FlipCard>
+        <div>
+          <div>1a</div>
+          <div>1b</div>
+          <div>1c</div>
+        </div>
+        <img
+          style={{ height: '80%', width: '80%' }}
+          src="https://cdn.pixabay.com/photo/2014/11/30/14/11/cat-551554_960_720.jpg"
+        ></img>
+      </FlipCard>
     </div>
   );
 }
--- src/components/ui/FlipCard.js
+++ src/components/ui/FlipCard.js
@@ -1,5 +1,52 @@
-const FlipCard = () => {
-  return null;
+import { useState } from 'react';
+import styled from 'styled-components';
+
+const CardBody = styled.div`
+  position: relative;
+  border: 2px solid black;
+  border-radius: 1rem;
+  height: 20rem;
+  width: 20rem;
+  transition: transform 0.3s ease;
+  transform-style: preserve-3d;
+
+  &.flipped {
+    transform: rotateY(180deg);
+  }
+`;
+const CardFront = styled.div`
+  position: absolute;
+  backface-visibility: hidden;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+const CardBack = styled.div`
+  position: absolute;
+  backface-visibility: hidden;
+  width: 100%;
+  height: 100%;
+  transform: rotateY(180deg);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const FlipCard = (props) => {
+  const [flipped, setFlipped] = useState(false);
+
+  const handleFlip = () => {
+    setFlipped((prev) => !prev);
+  };
+
+  return (
+    <CardBody className={flipped ? 'flipped' : ''} onClick={handleFlip}>
+      <CardFront>{props.children[0]}</CardFront>
+      <CardBack>{props.children[1]}</CardBack>
+    </CardBody>
+  );
 };
 
 export default FlipCard;
